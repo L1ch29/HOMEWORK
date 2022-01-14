24efc3578f491da599cd8919ec24933a018c329b
L1ch29 <vlad.sadovoy29@gmail.com>
2022-01-14 15:49:04 +0200

    Update homework_3.py

---
+++ Homework_3/homework_3.py
@@ -0,0 +1,91 @@
+triple_quotes=''' 
+●▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬●
+ ⠀⠀ ⠀    •●Welcome To Vlad's Calculator●•
+●▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬●
+'''
+print(triple_quotes)
+
+operation = input("""ஜ We have few options: +; -; /; *. ஜ
+Choose the one you like:""")
+
+#Operation +
+if operation == '+':
+    first_number = input("Enter first value: ")
+    if first_number.isdigit():
+        second_number = input("Second value: ")
+        if second_number.isdigit():
+            print(f"{first_number} + {second_number} = {int(first_number) + int(second_number)}")
+        else:
+            print(f"{second_number} is not digit")
+    elif first_number.isdigit():
+        second_number = input("Second value: ")
+        print(f"{first_number} + {second_number} = {first_number + second_number}")
+    else:
+        print(f"{first_number} is not digit or letter")
+
+
+
+#Operation -
+if operation == '-':
+    first_number = input("Enter first value: ")
+    if first_number.isdigit():
+        second_number = input("Second value: ")
+        if second_number.isdigit():
+            print(f"{first_number} - {second_number} = {int(first_number) - int(second_number)}")
+        else:
+            print(f"{second_number} is not digit")
+    elif first_number.isdigit():
+        second_number = input("Second value: ")
+        print(f"{first_number} - {second_number} = {first_number - second_number}")
+    else:
+        print(f"{first_number} is not digit or letter")
+
+
+
+
+
+#Operation *
+if operation == '*':
+    first_number = input("Enter first value: ")
+    if first_number.isdigit():
+        second_number = input("Second value: ")
+        if second_number.isdigit():
+            print(f"{first_number} * {second_number} = {int(first_number) * int(second_number)}")
+        else:
+            print(f"{second_number} is not digit")
+    elif first_number.isdigit():
+        second_number = input("Second value: ")
+        print(f"{first_number} * {second_number} = {first_number * second_number}")
+    else:
+        print(f"{first_number} is not digit or letter")
+
+
+
+#Operation /
+if operation == '/':
+    first_number = input("Enter first value: ")
+    if first_number.isdigit():
+        second_number = input("Second value: ")
+        if second_number.isdigit():
+            print(f"{first_number} / {second_number} = {int(first_number) / int(second_number)}")
+        else:
+            print(f"{second_number} is not digit")
+    elif first_number.isdigit():
+        second_number = input("Second value: ")
+        print(f"{first_number} / {second_number} = {first_number / second_number}")
+    else:
+        print(f"{first_number} is not digit or letter")
+
+
+
+elif operation == '//':
+    print("// is not developed yet'⌒' ")
+
+elif operation == '%':
+    print("% is not developed yet'⌒' ")
+
+
+
+
+
+
