f66cffcc67f33924c9659ddb1fc70c99e951b731
L1ch29 <vlad.sadovoy29@gmail.com>
2022-01-30 20:19:58 +0200

    Update homework_5.py

--- Homework_5/homework_5.py
+++ Homework_5/homework_5.py
@@ -1,4 +1,16 @@
-#Task
+#Task 2
+#2.1
+list = []
+index = 0
+while index < 10:
+    list.append(random.randint(0, 100))
+    index += 1
+print(list)
+
+index=0
+while index < len (list):
+    print(list[index])
+    index += 1
 
 
 
