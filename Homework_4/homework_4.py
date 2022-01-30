37926145899fa24a13b6ac6e2f3a5860d8cb1246
L1ch29 <vlad.sadovoy29@gmail.com>
2022-01-30 19:41:55 +0200

    homework_4

---
+++ Homework_4/homework_4.py
@@ -0,0 +1,55 @@
+#Task 2
+list= (1, 9, 10, 90, 100, 900, 1000, 9000)
+index=0
+summ=0
+while index<len(list):
+    summ=summ+list[index]
+    index=index+1
+print("summ =",summ)
+
+#Task 3
+list = [False, 0, 'str', '123', 'hello', '%', 1.2, 1]
+index = 0
+while True:
+    if index == len(list):
+        break
+    if type(list[index]) == str:
+        index += 1
+        continue
+    print(type(list[index]))
+    index += 1
+
+#Task 4
+#1
+list_1= [90, 68, 23, 73, 45, 9, 100]
+ln = list_1[0] if list_1 else None
+for index in list_1:
+    if index>ln:
+        ln=index
+print("Largest element is: ",ln)
+
+list_2=[38, 9, 45, 4, 85, 150, 23 ]
+ln = list_2[0] if list_2 else None
+for index in list_2:
+    if index>ln:
+        ln=index
+print("Largest element is: ",ln)
+
+
+#2
+list_1= [90, 68, 23, 73, 45, 9, 100]
+list_2=[38, 9, 45, 4, 85, 150, 23 ]
+list_3= []
+for index in list_1:
+    for j in list_2:
+        if index == j:
+            list_3.append(index)
+            break
+print(list_3)
+
+
+
+
+
+
+
