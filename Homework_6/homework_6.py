e9c4f0c0a48e5f024ba94088e2934847053f7778
L1ch29 <vlad.sadovoy29@gmail.com>
2022-02-06 19:51:58 +0200

    Update homework_6.py

---
+++ Homework_6/homework_6.py
@@ -0,0 +1,34 @@
+import random
+import math
+#Task 2
+dad={'Name':'Michael',
+     'Age':38,
+     'Sex':'male',
+     'Phone':'555-111-3333'}
+
+mum={'Name':'Clara',
+     'Age':30,
+     'Sex':'female',
+     'Phone':'999-333-1111'}
+
+son={'Name':'Kobe',
+     'Age':9,
+     'Sex':'male',
+     'Phone':'777-222-4444'}
+
+dog={'Name':'Oscar',
+     'Age':2,
+     'Sex':'male',
+     'Phone':'regular dog number' }
+
+all_family= {
+     'Michael': dad,
+     'Clara': mum,
+     'Kobe':son,
+     'Oscar':dog
+     }
+print(all_family)
+
+
+
+
