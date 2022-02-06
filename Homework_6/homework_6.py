f8665f1e5c1b5d496b9014c1acdc65be18d50598
L1ch29 <vlad.sadovoy29@gmail.com>
2022-02-06 22:13:03 +0200

    Update homework_6.py

--- Homework_6/homework_6.py
+++ Homework_6/homework_6.py
@@ -6,6 +6,7 @@ members_dict = {
        'Name':'Michael',
        'Age':38,
        'Sex':'male',
+       'Nationality':'American',
        'Phone':'555-111-3333',
        'Profession':'Director'},
 
@@ -13,6 +14,7 @@ members_dict = {
        'Name':'Clara',
         'Age':30,
         'Sex':'female',
+        'Nationality':'Jewish',
         'Phone':'999-333-1111',
         'Profession':'Accountant'},
 
@@ -20,19 +22,29 @@ members_dict = {
         'Name':'Kobe',
         'Age':9,
         'Sex':'male',
+        'Nationality':'American-Jew',
         'Phone':'777-222-4444',
-        'Profession':'student'},
+        'Profession':'Academy student num.5'},
 
     'Dog':{
          'Name':'Oscar',
          'Age':2,
          'Sex':'male',
-         'Breed': 'Labrador'}
+         'Breed': 'Labrador',
+         'Nationality':'Canadian',
+    }
 }
 print(len(members_dict))
-
-
 
 for member, member_info in members_dict.items():
      print(f"{member}:{member_info}")
 
+
+#Task 4
+element_to_find= input('₪Enter value or key which u want to search₪ :')
+
+for member, member_info in members_dict.items():
+     member_info_list= list(member_info.items())
+     for info_element in member_info_list:
+          if element_to_find in info_element:
+               print(f"{element_to_find} was found in \n{member}:{member_info}")
