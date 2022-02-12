257bbca5b702a9f8c458c2ad742876a9b0483bd9
L1ch29 <vlad.sadovoy29@gmail.com>
2022-02-12 18:22:39 +0200

    Update homework_5.py

--- Homework_5/homework_5.py
+++ Homework_5/homework_5.py
@@ -8,14 +8,11 @@ import random
 # def get_unique_numbers(numbers):
 #     list_of_unique_numbers = []
 #     unique_numbers = set(numbers)
-#
 #     for number in unique_numbers:
 #         list_of_unique_numbers.append(number)
-#
-#     return list_of_unique_numbers
-#
+#         return list_of_unique_numbers
 # print(get_unique_numbers(numbers))
-#
+
 # list_2 = ([random.randint(0, 10)
 # for element in range(0, 10)])
 # print(list_2)
@@ -23,20 +20,17 @@ import random
 # def get_unique_numbers(numbers):
 #     list_of_unique_numbers = []
 #     unique_numbers = set(numbers)
-#
 #     for number in unique_numbers:
 #         list_of_unique_numbers.append(number)
-#
-#     return list_of_unique_numbers
-#
+#         return list_of_unique_numbers
 # print(get_unique_numbers(numbers))
 #
-
+#
 # 1.2
 # list_3= []
 # for index in list_1:
-#     for j in list_2:
-#         if index == j:
+#     for element in list_2:
+#         if index == element:
 #             list_3.append(index)
 #             break
 # print(list_3)
