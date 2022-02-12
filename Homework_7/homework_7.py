6c874a76d4fa42d403767798fbf4eee1e144fc7b
L1ch29 <vlad.sadovoy29@gmail.com>
2022-02-12 18:12:29 +0200

    Update homework_7.py

--- Homework_7/homework_7.py
+++ Homework_7/homework_7.py
@@ -4,10 +4,10 @@ def max_number(number):
     ln = list[0] if list else None
     for index in number:
         if index> ln:
-            ln=index
+            ln= index
     return ln
 
-list = [10, 20, 30, 40, 50]
+list= [10, 20, 30, 40, 50]
 list_with_max= max_number(list)
 print(list_with_max)
 
@@ -16,12 +16,13 @@ def min_number(number):
     ln = list[0] if list else None
     for index in number:
         if index< ln:
-            len=index
+            len= index
     return ln
 
-list = [10, 20, 30, 40, 50]
+list= [10, 20, 30, 40, 50]
 list_with_min= min_number(list)
 print(list_with_min)
+
 
 
 
