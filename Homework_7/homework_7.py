3e07d3a46d4634f9163f7680bbcbf7251a7ca9d0
L1ch29 <vlad.sadovoy29@gmail.com>
2022-02-18 19:06:39 +0200

    Update homework_7.py

--- Homework_7/homework_7.py
+++ Homework_7/homework_7.py
@@ -25,5 +25,79 @@ print(list_with_min)
 
 
 
+#Task 4                                     VLAD'S NEW CALCULATOR
+
+def welcome():
+    return ("""
+ ●▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬●
+  ⠀⠀ ⠀    •●Welcome To Vlad's Calculator●•
+ ●▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬●
+ """)
+
+# Operation +
+def addition(x, y):
+   return x + y;
+# Operation -
+def subtraction(x, y):
+   return x - y;
+# Operation *
+def multiplication(x, y):
+   return x * y;
+# Operation /
+def division(x, y):
+   return x / y;
+# Operation %
+def result_of_division(x, y):
+    return x % y
+# Operation //
+def integer_division(x, y):
+    return x // y
+# Operation **
+def exponentiation(x, y):
+    return x ** y
+
+print("""
+ ●▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬●
+  ⠀⠀ ⠀    •●Welcome To Vlad's Calculator●•
+ ●▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬●
+ """)
+
+print("ஜWe have few optionsஜ, choose the one you like: ")
+print("1. Addition +")
+print("2. Subtraction -")
+print("3. Multiplication *")
+print("4. Division /")
+print("5. Result of division %")
+print("6. Integer division //")
+print("7. Exponentiation **")
+operation = input("Enter operation(1,2,3,4,5,6,7): ")
+
+first_number = int(input("Enter first value: "))
+second_number = int(input("Enter second value: "))
+
+# Operation +
+if operation == '1':
+   print(first_number, "+", second_number, "=", addition(first_number, second_number ))
+# Operation -
+elif operation == '2':
+   print(first_number, "-", second_number, "=", subtraction(first_number, second_number ))
+# Operation *
+elif operation == '3':
+   print(first_number, "*", second_number, "=", multiplication(first_number, second_number ))
+# Operation /
+elif operation == '4':
+   print(first_number, "/", second_number, "=", division(first_number, second_number ))
+# Operation %
+elif operation == '5':
+   print(first_number, "%", second_number, "=", result_of_division(first_number, second_number ))
+# Operation //
+elif operation == '6':
+   print(first_number, "//", second_number, "=", integer_division(first_number, second_number ))
+# Operation **
+elif operation == '7':
+   print(first_number, "**", second_number, "=", exponentiation(first_number, second_number ))
+
+else:
+    print('₪NOT VALID OPERATION₪')
 
 
